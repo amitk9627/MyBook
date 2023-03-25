dff38fce22ac659f2d27c3ca8f6a335111dc56a2
Amit Kumar <ramitgzp56@gmail.com>
2023-03-26 00:40:34 +0530

    pop up login

---
+++ index.js
@@ -0,0 +1,7 @@
+// login form pop-up
+document.getElementById("LOGIN").addEventListener("click",function (){
+    document.getElementById("myform").style.display="block";
+});
+document.getElementById("CLOSE").addEventListener("click",function(){
+    document.getElementById("myform").style.display="none";
+});
